36eda9229801632be6f539cbe8a27ee60571c779
Dylan Mascarenhas <dylanmascarenhas13@gmail.com>
2026-05-10 00:50:21 +0530

    Add Vercel Analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { TooltipProvider } from './components/ui/tooltip'
 import { AuthProvider } from './context/AuthContext'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.jsx'
+
+inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
